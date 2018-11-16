7003286eefc2ff495c09161f9de9313a6250da59
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-11-16 16:57:32 +0100

    Adding Cinema class. Constructor of this class create rows of chairs at movie theater. Adding main class for some tests

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,8 +1,50 @@
 package cinema;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cinema {
 
+    private final String cinemaName;
+    private List<Chair> chairs = new ArrayList<>();
 
+    public Cinema(String cinemaName, int numberOfRows, int chairsInRow){
+        this.cinemaName = cinemaName;
+
+        int rowMax = 'A' + (numberOfRows - 1);
+        for (char row = 'A'; row <= rowMax; row++) {
+            for (int chairNumber = 1; chairNumber <= chairsInRow; chairNumber++) {
+                Chair chair = new Chair(row + String.format("%02d", chairNumber));
+                chairs.add(chair);
+            }
+        }
+    }
+
+    public String getCinemaName(){
+        return cinemaName;
+    }
+
+    public boolean reserveChair(String chairNumber){
+        Chair requestChair = null;
+        for(Chair chair : chairs){
+            if(chair.getChairNumber().equals(chairNumber)){
+                requestChair = chair;
+                break;
+            }
+        }
+
+        if(requestChair == null){
+            System.out.println("There is no chair " + chairNumber );
+            return false;
+        }
+        return requestChair.reserve();
+    }
+
+    public void getChair() {
+        for(Chair chair : chairs){
+            System.out.println(chair.getChairNumber());
+        }
+    }
 
 
 }
--- src/cinema/Main.java
+++ src/cinema/Main.java
@@ -2,6 +2,19 @@ package cinema;
 
 public class Main {
     public static void main(String[] args) {
+        Cinema cinema = new Cinema("Lux Cinema", 8, 10);
+        cinema.getChair();
 
+        if(cinema.reserveChair("A08")){
+            System.out.println("Thank you for reservation");
+        }else{
+            System.out.println("Chair is already reserved");
+        }
+
+        if(cinema.reserveChair("A08")){
+            System.out.println("Thank you for reservation");
+        }else{
+            System.out.println("Please choose another chair");
+        }
     }
 }
