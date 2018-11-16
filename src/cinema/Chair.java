6d87461c3f259bf5ffadd68edf34d06a1a4ee91a
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-11-16 16:54:40 +0100

    Creating Chair class with reservation and cancel reservation methods.

--- src/cinema/Chair.java
+++ src/cinema/Chair.java
@@ -1,4 +1,34 @@
 package cinema;
 
 public class Chair {
+    private String chairNumber;
+    private boolean reserved = false;
+
+    public Chair(String chairNumber){
+        this.chairNumber = chairNumber;
+    }
+
+    public String getChairNumber() {
+        return chairNumber;
+    }
+
+    public boolean reserve(){
+        if(!this.reserved){
+            this.reserved = true;
+            System.out.println("Chair " + chairNumber + " reserved");
+            return true;
+        }else {
+            System.out.println("Chair " + chairNumber + " is already reserved");
+            return false;
+        }
+    }
+
+    public boolean cancel(){
+        if(this.reserved){
+            this.reserved = false;
+            System.out.println("This chair " + chairNumber + " is no longer reserved");
+            return true;
+        }
+        return false;
+    }
 }
